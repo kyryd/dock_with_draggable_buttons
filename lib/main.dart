a7debf5067816cf4f6af8b29fd891af0f60f5b08
Kyrylo <kirillgayduk@outlook.com>
2025-01-18 20:31:53 +0100

    Add  dock with draggable buttons, similar to the dock in MacOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+const kIconSize = 48.0;
+const kPadding = 4.0;
+const kMargin = 8.0;
+const kRadius = 8.0;
+
 void main() {
   runApp(const MyApp());
 }
@@ -7,119 +12,165 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        body: Center(
+          child: Dock(
+            items: ToolBarButton.icons,
+            builder: (e) => ToolBarButton(iconData: e),
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class Dock<T> extends StatefulWidget {
+  const Dock({
+    super.key,
+    required this.items,
+    required this.builder,
+  });
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  final List<T> items;
+  final Widget Function(T) builder;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Dock<T>> createState() => _DockState<T>();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _DockState<T> extends State<Dock<T>> {
+  late final List<T> _itemsBeforeDrag = widget.items.toList();
+  late final List<T> _items = widget.items.toList();
 
-  void _incrementCounter() {
+  T itemAtIndex(int index) => _itemsBeforeDrag[index];
+
+  int get sourceLength => widget.items.length;
+
+  bool get dragging => _items.length < sourceLength;
+
+  void _onAcceptWithDetails(DragTargetDetails<int> details, int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _items.insert(
+        index,
+        itemAtIndex(details.data),
+      );
+      _itemsBeforeDrag.clear();
+      _itemsBeforeDrag.addAll(_items);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(kRadius),
+        color: Colors.black12,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+      padding: const EdgeInsets.all(kPadding),
+      child: ConstrainedBox(
+        constraints: BoxConstraints(
+          minWidth: widget.items.length * kIconSize * 1.5,
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ..._items.asMap().entries.map((entry) {
+              int index = entry.key;
+              T item = entry.value;
+              return DragTarget<int>(
+                builder: (
+                  BuildContext context,
+                  List<dynamic> accepted,
+                  List<dynamic> rejected,
+                ) {
+                  return Draggable<int>(
+                      data: index,
+                      feedback: widget.builder(item),
+                      child: widget.builder(item),
+                      onDraggableCanceled: (velocity, offset) {
+                        setState(() {
+                          _items.clear();
+                          _items.addAll(_itemsBeforeDrag);
+                        });
+                      },
+                      onDragStarted: () => setState(() {
+                            _items.removeAt(index);
+                          }),
+                      onDragEnd: (details) {
+                        setState(() {
+                          if (!details.wasAccepted) {
+                            _items.clear();
+                            _items.addAll(_itemsBeforeDrag);
+                          }
+                        });
+                      });
+                },
+                onAcceptWithDetails: (DragTargetDetails<int> details) =>
+                    _onAcceptWithDetails(details, index),
+              );
+            }),
+            if (dragging)
+              DragTarget<int>(
+                builder: (
+                  BuildContext context,
+                  List<dynamic> accepted,
+                  List<dynamic> rejected,
+                ) =>
+                    Stub(),
+                onAcceptWithDetails: (DragTargetDetails<int> details) =>
+                    _onAcceptWithDetails(details, sourceLength - 1),
+              ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class Stub extends StatelessWidget {
+  const Stub({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: kIconSize,
+      height: kIconSize,
+    );
+  }
+}
+
+class ToolBarButton extends StatelessWidget {
+  const ToolBarButton({
+    super.key,
+    required final IconData iconData,
+  }) : _iconData = iconData;
+
+  final IconData _iconData;
+
+  static const icons = [
+    Icons.person,
+    Icons.message,
+    Icons.call,
+    Icons.camera,
+    Icons.photo,
+  ];
+
+  Color get backgroundColor => Colors.primaries[icons.indexOf(_iconData)];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: const BoxConstraints(minWidth: kIconSize),
+      height: kIconSize,
+      margin: const EdgeInsets.all(kMargin),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(kRadius),
+        color: backgroundColor,
+      ),
+      child: Center(child: Icon(_iconData, color: Colors.white)),
     );
   }
 }
